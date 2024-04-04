610a4c6796b861ad3a9b6b9b365a81987bf0ee9c
Sn0wix_ <sn0wix@proton.me>
2024-04-04 22:01:32 +0200

    todo update

--- src/main/java/net/sn0wix_/modObserverPlugin/ModObserverPlugin.java
+++ src/main/java/net/sn0wix_/modObserverPlugin/ModObserverPlugin.java
@@ -11,7 +11,6 @@ import java.util.logging.Logger;
 
 public final class ModObserverPlugin extends JavaPlugin {
     public static Logger LOGGER;
-    //asdf
     public static FileConfiguration CONFIG;
     public static final String MOD_ID = "mod_observer";
 

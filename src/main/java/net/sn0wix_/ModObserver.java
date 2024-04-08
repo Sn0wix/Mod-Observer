2966b06b60e3e5c7227e7656e8b102066d839f3f
Sn0wix_ <sn0wix@proton.me>
2024-04-08 21:20:27 +0200

    update

--- src/main/java/net/sn0wix_/ModObserver.java
+++ src/main/java/net/sn0wix_/ModObserver.java
@@ -1,7 +1,6 @@
 package net.sn0wix_;
 
-import net.fabricmc.api.ClientModInitializer;
-
+import net.fabricmc.api.ModInitializer;
 import net.fabricmc.fabric.api.client.networking.v1.*;
 import net.fabricmc.fabric.api.networking.v1.PacketByteBufs;
 import net.fabricmc.loader.api.FabricLoader;
@@ -11,16 +10,20 @@ import net.minecraft.util.Identifier;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+import javax.crypto.Cipher;
+import javax.crypto.spec.SecretKeySpec;
 import java.util.ArrayList;
+import java.util.Base64;
 import java.util.List;
 
-public class ModObserver implements ClientModInitializer {
+public class ModObserver implements ModInitializer {
+    //TODO fix mod loading?, fix enviroment crash
     public static final String MOD_ID = "mod_observer";
     public static final Logger LOGGER = LoggerFactory.getLogger("mod_observer");
-    public static final Identifier MODS_FOR_APPROVAL_PACKET = new Identifier(MOD_ID, "mods_for_approval");
+    public static Identifier MODS_FOR_APPROVAL_PACKET = new Identifier(MOD_ID, "mods_for_approval");
 
     @Override
-    public void onInitializeClient() {
+    public void onInitialize() {
         ClientConfigurationConnectionEvents.INIT.register((handler, client) -> ClientConfigurationNetworking.send(MODS_FOR_APPROVAL_PACKET, getModsBuf()));
     }
 
@@ -44,5 +47,12 @@ public class ModObserver implements ClientModInitializer {
             mods.add(modContainer.getMetadata().getId());
         }
         return mods;
+    }
+
+    public static String encrypt(String strToEncrypt, String secret) throws Exception {
+        SecretKeySpec secretKey = new SecretKeySpec(secret.getBytes(), "AES");
+        Cipher cipher = Cipher.getInstance("AES/ECB/PKCS5Padding");
+        cipher.init(Cipher.ENCRYPT_MODE, secretKey);
+        return Base64.getEncoder().encodeToString(cipher.doFinal(strToEncrypt.getBytes()));
     }
 }

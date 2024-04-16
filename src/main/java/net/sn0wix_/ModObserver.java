7570e6e0a39dd28c867825e5ed60e1ef4ac97ed7
Sn0wix_ <sn0wix@proton.me>
2024-04-16 16:31:01 +0200

    client initialization

--- src/main/java/net/sn0wix_/ModObserver.java
+++ src/main/java/net/sn0wix_/ModObserver.java
@@ -1,6 +1,6 @@
 package net.sn0wix_;
 
-import net.fabricmc.api.ModInitializer;
+import net.fabricmc.api.ClientModInitializer;
 import net.fabricmc.fabric.api.client.networking.v1.*;
 import net.fabricmc.fabric.api.networking.v1.PacketByteBufs;
 import net.fabricmc.loader.api.FabricLoader;
@@ -13,18 +13,19 @@ import org.slf4j.LoggerFactory;
 import java.util.ArrayList;
 import java.util.List;
 
-public class ModObserver implements ModInitializer {
+public class ModObserver implements ClientModInitializer {
     //TODO fix mod loading?, fix enviroment crash
     public static final String MOD_ID = "mod_observer";
     public static final Logger LOGGER = LoggerFactory.getLogger("mod_observer");
     public static Identifier MODS_FOR_APPROVAL_PACKET = new Identifier(MOD_ID, "mods_for_approval");
     public static Identifier MOD_REQUEST_PACKET = new Identifier(MOD_ID, "request_mods");
 
+
     @Override
-    public void onInitialize() {
+    public void onInitializeClient() {
         ClientConfigurationConnectionEvents.INIT.register((handler, client) -> ClientConfigurationNetworking.send(MODS_FOR_APPROVAL_PACKET, getModsBuf()));
 
-        ClientPlayNetworking.registerReceiver(MOD_REQUEST_PACKET, (client, handler, buf, responseSender) -> client.execute(() -> ClientConfigurationNetworking.send(MODS_FOR_APPROVAL_PACKET, getModsBuf())));
+        ClientPlayNetworking.registerGlobalReceiver(MOD_REQUEST_PACKET, (client, handler, buf, responseSender) -> client.execute(() -> ClientConfigurationNetworking.send(MODS_FOR_APPROVAL_PACKET, getModsBuf())));
     }
 
     private static PacketByteBuf getModsBuf() {

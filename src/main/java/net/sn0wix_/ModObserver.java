87efd5173aa49cc2eea22af05552a0e908455438
Sn0wix_ <sn0wix@proton.me>
2024-05-02 21:43:45 +0200

    loom update

--- src/main/java/net/sn0wix_/ModObserver.java
+++ src/main/java/net/sn0wix_/ModObserver.java
@@ -1,6 +1,7 @@
 package net.sn0wix_;
 
 import net.fabricmc.api.ClientModInitializer;
+import net.fabricmc.fabric.api.client.event.lifecycle.v1.ClientLifecycleEvents;
 import net.fabricmc.fabric.api.client.networking.v1.*;
 import net.fabricmc.fabric.api.networking.v1.PacketByteBufs;
 import net.fabricmc.loader.api.FabricLoader;

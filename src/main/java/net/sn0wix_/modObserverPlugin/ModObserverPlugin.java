1440bd2e87b5452d2a2cb10a55227ab5832e9839
Sn0wix_ <sn0wix@proton.me>
2025-07-18 12:08:56 +0200

    init

---
+++ src/main/java/net/sn0wix_/modObserverPlugin/ModObserverPlugin.java
@@ -0,0 +1,17 @@
+package net.sn0wix_.modObserverPlugin;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class ModObserverPlugin extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}

056413adf2d69b026271f24b5393866740535f67
Sn0wix_ <sn0wix@proton.me>
2025-08-07 14:59:03 +0200

    packet testing

--- src/main/java/net/sn0wix_/modObserverPlugin/ModObserverPlugin.java
+++ src/main/java/net/sn0wix_/modObserverPlugin/ModObserverPlugin.java
@@ -1,5 +1,12 @@
 package net.sn0wix_.modObserverPlugin;
 
+import com.comphenix.protocol.PacketType;
+import com.comphenix.protocol.ProtocolLibrary;
+import com.comphenix.protocol.ProtocolManager;
+import com.comphenix.protocol.events.ListenerPriority;
+import com.comphenix.protocol.events.PacketAdapter;
+import com.comphenix.protocol.events.PacketEvent;
+import io.papermc.paper.connection.PlayerConnection;
 import net.sn0wix_.modObserverPlugin.commands.ConfigurationCommand;
 import net.sn0wix_.modObserverPlugin.config.Config;
 import net.sn0wix_.modObserverPlugin.listeners.Events;
@@ -7,34 +14,76 @@ import net.sn0wix_.modObserverPlugin.networking.PacketHandler;
 import net.sn0wix_.modObserverPlugin.players.WaitingForResponsePlayers;
 import org.bukkit.ChatColor;
 import org.bukkit.configuration.file.FileConfiguration;
+import org.bukkit.entity.Player;
 import org.bukkit.plugin.Plugin;
 import org.bukkit.plugin.java.JavaPlugin;
+import org.bukkit.plugin.messaging.PluginMessageListener;
 import org.bukkit.scheduler.BukkitRunnable;
+import org.jetbrains.annotations.NotNull;
 
 import java.util.logging.Logger;
 
 public final class ModObserverPlugin extends JavaPlugin {
 
     /*https://minecraft.wiki/w/Minecraft_Wiki:Projects/wiki.vg_merge/Minecraft_Forge_Handshake
-    * Forge handshake emulation - config enabled
-    * S -> C mods wanted packet
-    * C -> S mods packet
-    *
-    * Json packet formatting
-    * serverside tampering detection - config enabled
-    * Mod jars hashes - config enabled
-    *
-    * Config generator tool
-    * */
+     * Forge handshake emulation - config enabled
+     * S -> C mods wanted packet - bl check tampering, bl send children, bl send hashes
+     * C -> S mods packet - json?
+     * S -> C kick (optional) - string message, json? mods
+     *
+     * Json packet formatting
+     * serverside tampering detection - config enabled
+     * Mod jars hashes - config enabled
+     *
+     * whitelist, blacklist, required mods jsons
+     * Config generator tool
+     * */
     public static Plugin PLUGIN;
     public static Logger LOGGER;
     public static FileConfiguration CONFIG;
     public static final String MOD_ID = "mod_observer";
     public static BukkitRunnable pluginTicker;
+    ProtocolManager manager;
 
     @Override
     public void onEnable() {
-        //Initialize logger
+        manager = ProtocolLibrary.getProtocolManager();
+
+        getServer().getMessenger().registerIncomingPluginChannel(this, "mod_observer:mods", new PluginMessageListener() {
+            @Override
+            public void onPluginMessageReceived(@NotNull String channel, @NotNull Player player, byte @NotNull [] message) {
+                LOGGER.info("RECEIVED");
+            }
+
+
+            @Override
+            public void onPluginMessageReceived(@NotNull String channel, @NotNull PlayerConnection connection, byte @NotNull [] message) {
+                LOGGER.info("RECEIVED");
+            }
+        });
+
+
+        manager.addPacketListener(new PacketAdapter(this, ListenerPriority.HIGHEST, PacketType.Configuration.Client.CUSTOM_PAYLOAD) {
+            @Override
+            public void onPacketReceiving(PacketEvent event) {
+                LOGGER.info("CUSTOM PAYLOAD PACKET");
+                LOGGER.info(event.getPacket().toString());
+                LOGGER.info(event.toString());
+                super.onPacketReceiving(event);
+            }
+        });
+
+        /*manager.addPacketListener(new PacketAdapter(this, ListenerPriority.HIGHEST, PacketType.Play.Client.CUSTOM_PAYLOAD) {
+            @Override
+            public void onPacketReceiving(PacketEvent event) {
+                LOGGER.info("CUSTOM PAYLOAD PACKET");
+                LOGGER.info(event.getPacket().toString());
+                LOGGER.info(event.toString());
+                super.onPacketReceiving(event);
+            }
+        });*/
+
+        /*//Initialize logger
         LOGGER = getLogger();
         LOGGER.info("----------------ModObserver----------------");
 
@@ -84,12 +133,12 @@ public final class ModObserverPlugin extends JavaPlugin {
                 "\nThis will add you to the Ignored players list, so you won't be checked.\nOnce you are in game, execute /modobserver help to view the help message." +
                 "\nAnd don't forget to remove yourself from the Ignored players list: /modobserver ignoredPlayers remove <yourName>");
         LOGGER.info("If this plugin gives you errors when someone is kicked, ignore them.");
-        LOGGER.info("----------------ModObserver----------------");
+        LOGGER.info("----------------ModObserver----------------");*/
     }
 
     @Override
     public void onDisable() {
-        //Config
+        /*//Config
         Config.saveValues(CONFIG);
         saveConfig();
 
@@ -99,6 +148,6 @@ public final class ModObserverPlugin extends JavaPlugin {
         //Plugin messaging
         getServer().getMessenger().unregisterIncomingPluginChannel(this, PacketHandler.MODS_FOR_APPROVAL_CHANNEL);
         getServer().getMessenger().unregisterOutgoingPluginChannel(this, PacketHandler.MODS_FOR_APPROVAL_CHANNEL);
-        LOGGER.info("ModObserver disabled!");
+        LOGGER.info("ModObserver disabled!");*/
     }
 }
--- src/main/java/net/sn0wix_/modObserverPlugin/listeners/Events.java
+++ src/main/java/net/sn0wix_/modObserverPlugin/listeners/Events.java
@@ -9,34 +9,35 @@ import org.bukkit.event.EventHandler;
 import org.bukkit.event.EventPriority;
 import org.bukkit.event.Listener;
 import org.bukkit.event.player.PlayerJoinEvent;
+import org.bukkit.event.player.PlayerLoginEvent;
 import org.bukkit.event.player.PlayerQuitEvent;
 
 public class Events implements Listener {
     //Lowest priority to keep the join message set to an empty string
-    @EventHandler(priority = EventPriority.LOWEST)
+    /*@EventHandler(priority = EventPriority.LOWEST)
     public void join(PlayerJoinEvent event) {
         if (!Util.checkIncomingPlayer(event.getPlayer())) {
             event.joinMessage(Component.empty());
         } else {
             IncomingPlayers.removePlayer(event.getPlayer().getName());
         }
-    }
+    }*/
 
 
-    @EventHandler
-    public void login(PlayerConnectionInitialConfigureEvent event) {
+    /*@EventHandler
+    public void login(PlayerLoginEvent event) {
         IncomingPlayers.addPlayer(event.getConnection().getProfile().getName());
 
         if (Config.IGNORED_PLAYERS.contains(event.getConnection().getProfile().getName())) {
             IncomingPlayers.setApproved(event.getConnection().getProfile().getName());
         }
-    }
+    }*/
 
-    @EventHandler(priority = EventPriority.LOWEST)
+    /*@EventHandler(priority = EventPriority.LOWEST)
     public void quit(PlayerQuitEvent event) {
         if (IncomingPlayers.containsPlayer(event.getPlayer().getName())) {
             IncomingPlayers.removePlayer(event.getPlayer().getName());
             event.quitMessage(Component.empty());
         }
-    }
+    }*/
 }
